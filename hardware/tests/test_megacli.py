71b12afe841b4866bbdb8df3005c683cbb821267
Frederic Lepied <frederic.lepied@enovance.com>
2014-11-30 22:02:20 +0100

    fix flake8 reports on test_megacli.py

--- hardware/tests/test_megacli.py
+++ hardware/tests/test_megacli.py
@@ -17,6 +17,8 @@
 
 import unittest
 
+from utils import sample
+
 from hardware import megacli
 
 
@@ -38,40 +40,16 @@ class TestMegacliTest(unittest.TestCase):
         self.assertEqual(megacli.parse_output(' a : b'), {'A': 'b'})
 
     def test_parse_output_adpcount(self):
-        self.assertEqual(megacli.parse_output(''' 
-Controller Count: 1.
- 
-Exit Code: 0x01'''), {'ControllerCount': 1,
-                      'ExitCode': '0x01'})
+        self.assertEqual(megacli.parse_output(sample('megacli_adpcount')),
+                         {'ControllerCount': 1,
+                          'ExitCode': '0x01'})
 
     def test_adp_count(self):
-        self.output = ''' 
-Controller Count: 1.
- 
-Exit Code: 0x01'''
+        self.output = sample('megacli_adpcount')
         self.assertEqual(megacli.adp_count(), 1)
 
     def test_adp_all_info(self):
-        self.output = '''megacli -adpallinfo -aALL
- 
-Adapter #0
- 
-==============================================================================
-                    Versions
-                ================
-Product Name    : PERC H710 Mini
-Serial No       : 29F026R
-FW Package Build: 21.1.0-0007
-
-                Device Present
-                ================
-Virtual Drives    : 1 
-  Degraded        : 0 
-  Offline         : 0 
-Physical Devices  : 7 
-  Disks           : 6 
-  Critical Disks  : 0 
-  Failed Disks    : 0 '''
+        self.output = sample('megacli_adp_all_info')
         self.assertEqual(megacli.adp_all_info(0),
                          {'CriticalDisks': 0,
                           'Degraded': 0,
@@ -146,157 +124,39 @@ PD Type: SAS'''
         self.assertEqual(megacli.ld_get_num(0), 1)
 
     def test_ld_get_info(self):
-        self.output = '''
-Adapter 0 -- Virtual Drive Information:
-Virtual Drive: 0 (Target Id: 0)
-Name                :
-RAID Level          : Primary-1, Secondary-0, RAID Level Qualifier-0
-Size                : 465.25 GB
-Sector Size         : 512
-Mirror Data         : 465.25 GB
-State               : Optimal
-Strip Size          : 64 KB
-Number Of Drives    : 2
-Span Depth          : 1
-Default Cache Policy: WriteBack, ReadAdaptive, Direct, No Write Cache if Bad BBU
-Current Cache Policy: WriteBack, ReadAdaptive, Direct, No Write Cache if Bad BBU
-Default Access Policy: Read/Write
-Current Access Policy: Read/Write
-Disk Cache Policy   : Disk's Default
-Encryption Type     : None
-Default Power Savings Policy: Controller Defined
-Current Power Savings Policy: None
-Can spin up in 1 minute: Yes
-LD has drives that support T10 power conditions: No
-LD's IO profile supports MAX power savings with cached writes: No
-Bad Blocks Exist: No
-Is VD Cached: Yes
-Cache Cade Type : Read Only'''
+        self.output = sample('megacli_ld_get_info')
         self.assertEqual(megacli.ld_get_info(0, 0),
                          {'Adapter0--VirtualDriveInformation': '',
                           'BadBlocksExist': 'No',
                           'CacheCadeType': 'Read Only',
                           'CanSpinUpIn1Minute': 'Yes',
                           'CurrentAccessPolicy': 'Read/Write',
-                          'CurrentCachePolicy': 'WriteBack, ReadAdaptive, Direct, No Write Cache if Bad BBU',
+                          'CurrentCachePolicy':
+                          'WriteBack, ReadAdaptive, Direct, '
+                          'No Write Cache if Bad BBU',
                           'CurrentPowerSavingsPolicy': 'None',
                           'DefaultAccessPolicy': 'Read/Write',
-                          'DefaultCachePolicy': 'WriteBack, ReadAdaptive, Direct, No Write Cache if Bad BBU',
+                          'DefaultCachePolicy': 'WriteBack, ReadAdaptive, '
+                          'Direct, No Write Cache if Bad BBU',
                           'DefaultPowerSavingsPolicy': 'Controller Defined',
                           'DiskCachePolicy': "Disk's Default",
                           'EncryptionType': 'None',
                           'IsVdCached': 'Yes',
-                          "Ld'SIoProfileSupportsMaxPowerSavingsWithCachedWrites": 'No',
+                          "Ld'SIoProfileSupportsMaxPowerSavings"
+                          "WithCachedWrites": 'No',
                           'LdHasDrivesThatSupportT10PowerConditions': 'No',
                           'MirrorData': '465.25 GB',
                           'Name': '',
                           'NumberOfDrives': 2,
-                          'RaidLevel': 'Primary-1, Secondary-0, RAID Level Qualifier-0',
+                          'RaidLevel': 'Primary-1, Secondary-0, RAID Level '
+                          'Qualifier-0',
                           'SectorSize': 512,
                           'Size': '465.25 GB',
                           'SpanDepth': 1,
                           'State': 'Optimal',
                           'StripSize': '64 KB'})
 
-ENC_OUTPUT = '''
-                                     
-    Number of enclosures on adapter 0 -- 2
-
-    Enclosure 0:
-    Device ID                     : 6
-    Number of Slots               : 12
-    Number of Power Supplies      : 2
-    Number of Fans                : 3
-    Number of Temperature Sensors : 1
-    Number of Alarms              : 1
-    Number of SIM Modules         : 0
-    Number of Physical Drives     : 2
-    Status                        : Normal
-    Position                      : 1
-    Connector Name                : Port 0 - 3
-    Enclosure type                : SES
-    FRU Part Number               : N/A
-    Enclosure Serial Number       : N/A 
-    ESM Serial Number             : N/A 
-    Enclosure Zoning Mode         : N/A 
-    Partner Device Id             : 65535
-
-    Inquiry data                  :
-        Vendor Identification     : LSI CORP
-        Product Identification    : SAS2X28         
-        Product Revision Level    : 0717
-        Vendor Specific           : x36-55.7.23.0       
-
-Number of Voltage Sensors         :2
-
-Voltage Sensor                    :0
-Voltage Sensor Status             :OK
-Voltage Value                     :5070 milli volts
-
-Voltage Sensor                    :1
-Voltage Sensor Status             :OK
-Voltage Value                     :11910 milli volts
-
-Number of Power Supplies     : 2 
-
-Power Supply                 : 0 
-Power Supply Status          : Not Installed
-
-Power Supply                 : 1 
-Power Supply Status          : Not Installed
-
-Number of Fans               : 3 
-
-Fan                          : 0 
-Fan Status                   : OK
-
-Fan                          : 1 
-Fan Speed              :Medium Speed
-Fan Status                   : OK
-
-Fan                          : 2 
-Fan Speed              :Medium Speed
-Fan Status                   : OK
-
-Number of Temperature Sensors : 1 
-
-Temp Sensor                  : 0 
-Temperature                  : 25 
-Temperature Sensor Status    : OK
-
-Number of Chassis             : 1 
-
-Chassis                      : 0 
-Chassis Status               : OK
-
-    Enclosure 1:
-    Device ID                     : 252
-    Number of Slots               : 8
-    Number of Power Supplies      : 0
-    Number of Fans                : 0
-    Number of Temperature Sensors : 0
-    Number of Alarms              : 0
-    Number of SIM Modules         : 1
-    Number of Physical Drives     : 0
-    Status                        : Normal
-    Position                      : 1
-    Connector Name                : Unavailable
-    Enclosure type                : SGPIO
-    FRU Part Number               : N/A
-    Enclosure Serial Number       : N/A 
-    ESM Serial Number             : N/A 
-    Enclosure Zoning Mode         : N/A 
-    Partner Device Id             : Unavailable
-
-    Inquiry data                  :
-        Vendor Identification     : LSI     
-        Product Identification    : SGPIO           
-        Product Revision Level    : N/A 
-        Vendor Specific           :                     
-
-
-Exit Code: 0x00
-'''
+ENC_OUTPUT = sample('megacli_enc')
 
 if __name__ == "__main__":
     unittest.main()
