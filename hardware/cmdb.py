0355daec96951de8558b58501e6660ed03dc4cce
Frederic Lepied <frederic.lepied@enovance.com>
2014-12-01 22:39:08 +0100

    added cmdb.py

---
+++ hardware/cmdb.py
@@ -0,0 +1,112 @@
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Author: Frederic Lepied <frederic.lepied@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+'''
+'''
+
+import errno
+import logging
+import pprint
+import shutil
+
+from generate import is_included
+
+
+def cmdb_filename(cfg_dir, name):
+    'Return the cmdb filename.'
+    return cfg_dir + name + '.cmdb'
+
+
+def load_cmdb(cfg_dir, name):
+    'Load the cmdb.'
+    filename = cmdb_filename(cfg_dir, name)
+    try:
+        if "generate(" in open(filename).read(20):
+            shutil.copy2(filename, filename + ".orig")
+        return eval(open(filename).read(-1))
+    except IOError as xcpt:
+        if xcpt.errno != errno.ENOENT:
+            logging.error("exception while processing CMDB %s" % str(xcpt))
+        return None
+
+
+def save_cmdb(cfg_dir, name, cmdb):
+    'Save the cmdb.'
+    filename = cmdb_filename(cfg_dir, name)
+    try:
+        pprint.pprint(cmdb, stream=open(filename, 'w'))
+    except IOError as xcpt:
+        logging.error("exception while saving CMDB %s" % str(xcpt))
+
+_ERROR_CB = None
+
+
+def set_warning_error(cb):
+    'Define the callback to be used by warning_error'
+    global _ERROR_CB
+    _ERROR_CB = cb
+
+
+def warning_error(error):
+    'Use the callback set by set_warning_error or log the error'
+    if _ERROR_CB:
+        _ERROR_CB(error)
+    else:
+        logging.error(error)
+
+
+def update_cmdb(cmdb, var, pref, forced_find):
+    '''Handle CMDB settings if present.
+
+CMDB is updated with var. var is also augmented with the cmdb entry
+found.
+'''
+
+    def update_entry(entry, cmdb, idx):
+        'Update var using a cmdb entry and save the full cmdb on disk.'
+        entry.update(var)
+        var.update(entry)
+        var['used'] = 1
+        cmdb[idx] = var
+
+    # First pass to lookup if the var is already in the database
+    # and if this is the case, reuse the entry.
+    idx = 0
+    for entry in cmdb:
+        if is_included(pref, entry):
+            update_entry(entry, cmdb, idx)
+            break
+        idx += 1
+    else:
+        # not looking for $$ type matches
+        if not forced_find:
+            # Second pass, find a not used entry.
+            idx = 0
+            for entry in cmdb:
+                if 'used' not in entry:
+                    update_entry(entry, cmdb, idx)
+                    break
+                idx += 1
+            else:
+                warning_error("No more entry in the CMDB, aborting.")
+                return False
+        else:
+            warning_error("No entry matched in the CMDB, aborting.")
+            return False
+    return True
+
+# cmdb.py ends here
---
+++ hardware/tests/test_cmdb.py
@@ -0,0 +1,67 @@
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Author: Frederic Lepied <frederic.lepied@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import unittest
+
+from hardware import cmdb
+
+
+class TestCmdb(unittest.TestCase):
+
+    def test_update_cmdb_simple(self):
+        data = [{'b': 1}]
+        var = {'a': 1}
+        result = cmdb.update_cmdb(data, var, var, False)
+        self.assertTrue(result, cmdb)
+        self.assertEqual(data, [{'a': 1, 'b': 1, 'used': 1}])
+        self.assertEqual(var, {'a': 1, 'b': 1, 'used': 1})
+
+    def test_update_cmdb_reuse(self):
+        data = [{'a': 1, 'used': 1}]
+        var = {'a': 1}
+        result = cmdb.update_cmdb(data, var, var, False)
+        self.assertTrue(result, cmdb)
+        self.assertEqual(data, [{'a': 1, 'used': 1}])
+        self.assertEqual(var, {'a': 1, 'used': 1})
+
+    def test_update_cmdb_reuse2(self):
+        data = [{'a': 1, 'b': 1, 'c': 1, 'used': 1}]
+        cmdb_result = [{'a': 1, 'b': 2, 'c': 1, 'used': 1}]
+        var = {'a': 1, 'b': 2}
+        pref = {'a': 1}
+        result = cmdb.update_cmdb(data, var, pref, False)
+        self.assertTrue(result, cmdb_result)
+        self.assertEqual(data, cmdb_result)
+        self.assertEqual(var, {'a': 1, 'b': 2, 'c': 1, 'used': 1})
+
+    def test_update_cmdb_full(self):
+        data = [{'a': 2, 'used': 1}]
+        var = {'a': 1}
+        result = cmdb.update_cmdb(data, var, var, False)
+        self.assertFalse(result, cmdb)
+
+    def test_update_cmdb_full2(self):
+        data = [{'a': 'ff:ff'}]
+        var = {'a': 'FF:FF'}
+        result = cmdb.update_cmdb(data, var, var, True)
+        self.assertFalse(result, cmdb)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# test_cmdb.py ends here
