redhat-cip/hardware

commit 4f4fcd2802ef2ee15ff11a687ebae5eeb410de53

cardiff: Fixing gnuplot header
Since commit 84cb10a5c96a6ed03bd39141a455c733e0932cbc, the gnuplot call
was avoiding the "\n" to be expanded properly. This patch add the \n
support back to title & subtitle.

This patch does insure the \n to be expanded making the gnuplot header
readable again.

hardware/cardiff/cardiff.py
@@ -302,7 +302,7 @@ def do_plot(current_dir, gpm_dir, main_title, subtitle, name, unit, titles,
                              (expected_value, expected_value))
             myfile.write("\n")
 
-        f.write("call \'%s/graph2D.gpm\' \'%s' \'%s\' \'%s\' \'%s\' \'%s\' "
+        f.write("call \'%s/graph2D.gpm\' \"%s" \"%s\" \'%s\' \'%s\' \'%s\' "
                 "\'%s\'\n" % (current_dir, main_title, subtitle,
                               current_dir + "/" + name + ".plot",
                               name, current_dir + name, unit))

commit ca83a6a42386ae6bc57439705776f40ffc44eb4e

cardiff: Fixing typo
Commit 4f4fcd2802ef2ee15ff11a687ebae5eeb410de53 introduced a fatal typo.

hardware/cardiff/cardiff.py
@@ -305,7 +305,7 @@ def do_plot(current_dir, gpm_dir, main_title, subtitle, name, unit, titles,
                              (expected_value, expected_value))
             myfile.write("\n")
 
-        f.write("call \'%s/graph2D.gpm\' \"%s" \"%s\" \'%s\' \'%s\' \'%s\' "
+        f.write("call \'%s/graph2D.gpm\' \"%s\" \"%s\" \'%s\' \'%s\' \'%s\' "
                 "\'%s\'\n" % (current_dir, main_title, subtitle,
                               current_dir + "/" + name + ".plot",
                               name, current_dir + name, unit))