e752b950eabbe14cff99ccb3e08a557d3526317f
Cédric Jeanneret <cjeanner@redhat.com>
2018-07-10 12:43:59 +0200

    New task in hardware detection: detect RTC setting.
    This will allow to add a new validation before deploying so that
    tripleo won't deploy anything until hardware clocks are set to RTC.
    
    This setting is mandatory in order to prevent any issues regarding
    timezone, future-dated files and so on.
    
    More information: https://storyboard.openstack.org/#!/story/2002926
    
    Story: 2002926

--- hardware/detect.py
+++ hardware/detect.py
@@ -45,6 +45,7 @@ from hardware import hpacucli
 from hardware import infiniband as ib
 from hardware import ipmi
 from hardware import megacli
+from hardware import rtc
 
 SIOCGIFNETMASK = 0x891b
 
@@ -755,6 +756,10 @@ def detect_temperatures(hwlst):
                                processor_num, "critical_alarm")
 
 
+def detect_rtc_clock(hw_lst):
+    hw_lst.append(('system', 'rtc', 'utc', rtc.get_rtc()))
+
+
 def parse_ahci(hrdw, words):
     if len(words) < 4:
         return
@@ -805,6 +810,7 @@ def _main(options):
     detect_temperatures(hrdw)
     detect_utils.get_ddr_timing(hrdw)
     detect_utils.ipmi_sdr(hrdw)
+    detect_rtc_clock(hrdw)
     _, output = cmd("dmesg")
     parse_dmesg(hrdw, output)
 
