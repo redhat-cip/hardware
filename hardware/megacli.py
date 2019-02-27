redhat-cip/hardware

commit 54a78530fca3bfe81cfa54fc96e19ee7688280ca

quality: Improving coding style
This patch is about removing many coding style issues and improve the global score :

	Your code has been rated at 9.06/10 (previous run: 8.83/10, +0.24)

This patch makes pylint & pep8 no more failing.

Signed-off-by: Erwan Velu <e.velu@criteo.com>

hardware/megacli.py
@@ -42,9 +42,8 @@ def which(cmd, mode=os.F_OK | os.X_OK, path=None):
     # Check that a given file can be accessed with the correct mode.
     # Additionally check that `file` is not a directory, as on Windows
     # directories pass the os.access check.
-    def _access_check(fn, mode):
-        return (os.path.exists(fn) and os.access(fn, mode)
-                and not os.path.isdir(fn))
+    def _access_check(myfile, mode):
+        return os.path.exists(myfile and os.access(myfile, mode) and not os.path.isdir(myfile))
 
     # If we're given a path with a directory part, look it up directly rather
     # than referring to PATH directories. This includes checking relative to
@@ -68,12 +67,12 @@ def which(cmd, mode=os.F_OK | os.X_OK, path=None):
     files = [cmd]
 
     seen = set()
-    for dir in path:
-        normdir = os.path.normcase(dir)
+    for directory in path:
+        normdir = os.path.normcase(directory)
         if normdir not in seen:
             seen.add(normdir)
             for thefile in files:
-                name = os.path.join(dir, thefile)
+                name = os.path.join(directory, thefile)
                 if _access_check(name, mode):
                     return name
     return None
@@ -130,9 +129,9 @@ def run_megacli(*args):
         cmd = prog_exec + ' - ' + ' '.join(args)
         proc = Popen(cmd, shell=True, stdout=PIPE, universal_newlines=True)
         return proc.communicate()[0]
-    else:
-        sys.stderr.write('Cannot find megacli on the system\n')
-        return ""
+
+    sys.stderr.write('Cannot find megacli on the system\n')
+    return ""
 
 
 def run_and_parse(*args):
@@ -149,8 +148,7 @@ def adp_count():
     arr = run_and_parse('adpCount')
     if 'ControllerCount' in arr:
         return int(arr['ControllerCount'])
-    else:
-        return 0
+    return 0
 
 
 def adp_all_info(ctrl):

hardware/areca.py
@@ -29,9 +29,9 @@ def split_units(lis):
     'If the value have unit, remove it from the value and return the unit name'
     for unit in ['RPM', '%', ' V', ' C', 'Seconds', 'Times', 'MHz', 'KB', 'MB',
                  'GB']:
-        m = re.search("(.*)%s$" % unit, lis[1])
-        if m:
-            lis[1] = m.group(1).replace(' ', '')
+        match = re.search("(.*)%s$" % unit, lis[1])
+        if match:
+            lis[1] = match.group(1).replace(' ', '')
             return unit.replace(' ', '')
     return None
 
@@ -49,9 +49,9 @@ def parse_output(output, rev=False):
         if len(lis) == 2:
             if "GuiErrMsg" in lis[0]:
                 continue
-            m = re.search('\[(Enclosure#.*)', lis[0])
-            if m:
-                append = m.group(1).replace('#', '') + "/"
+            match = re.search('\[(Enclosure#.*)', lis[0])
+            if match:
+                append = match.group(1).replace('#', '') + "/"
                 continue
             if append:
                 lis[0] = "%s%s" % (append, lis[0])

hardware/benchmark/disk.py
@@ -35,8 +35,7 @@ RAMP_TIME = 5
 
 def is_booted_storage_device(disk):
     """Check if a given disk is booted."""
-    cmdline = "grep -w /ahcexport /proc/mounts | cut -d ' ' -f 1 "
-    "| sed -e 's/[0-9]*//g'"
+    cmdline = "grep -w /ahcexport /proc/mounts | cut -d ' ' -f 1 | sed -e 's/[0-9]*//g'"
     if '/dev/' not in disk:
         disk = '/dev/%s' % disk
     grep_cmd = subprocess.Popen(cmdline,

hardware/benchmark/mem.py
@@ -27,10 +27,11 @@ from hardware.benchmark import utils
 
 def get_available_memory():
     """Return the total amount of available memory, in bytes."""
-    with open('/proc/meminfo', 'r') as f:
-        for line in f:
+    with open('/proc/meminfo', 'r') as meminfo:
+        for line in meminfo:
             if line.startswith('MemFree:'):
                 return int(line.split()[1]) * 1024
+    return -1
 
 
 def check_mem_size(block_size, cpu_count):

hardware/bios_hp.py
@@ -27,8 +27,8 @@ import xml.etree.ElementTree as ET
 from hardware.detect_utils import cmd
 
 
-def get_hp_conrep(hw_lst):
-    for i in hw_lst:
+def get_hp_conrep(hwlst):
+    for i in hwlst:
         if i[0:3] == ('system', 'product', 'vendor'):
             if i[3] not in ['HPE', 'HP']:
                 return True, ""
@@ -42,23 +42,23 @@ def get_hp_conrep(hw_lst):
     return True, return_value
 
 
-def dump_hp_bios(hw_lst):
+def dump_hp_bios(hwlst):
     # handle output injection for testing purpose
-    valid, hp_config = get_hp_conrep(hw_lst)
+    valid, hpconfig = get_hp_conrep(hwlst)
     if not valid:
         return False
 
-    if hp_config:
-        xml = ET.fromstring(hp_config)
+    if hpconfig:
+        xml = ET.fromstring(hpconfig)
         root = xml.iter("Section")
 
         for child in root:
-            hw_lst.append(('hp', 'bios', child.attrib['name'], child.text))
+            hwlst.append(('hp', 'bios', child.attrib['name'], child.text))
 
     return True
 
 
 if __name__ == "__main__":
-    hw_lst = []
-    dump_hp_bios(hw_lst)
-    pprint.pprint(hw_lst)
+    hwlst = []
+    dump_hp_bios(hwlst)
+    pprint.pprint(hwlst)

hardware/cardiff/cardiff.py
@@ -164,16 +164,16 @@ def analyze_data(global_params, pattern, ignore_list, detail, rampup_value=0,
         sys.exit(2)
 
     health_data_file = utils.find_file(path, pattern)
-    if len(health_data_file) == 0:
+    if not health_data_file:
         print("No log file found with pattern %s!" % pattern)
         sys.exit(1)
+
+    if rampup_value == 0:
+        print("### %d files Selected with pattern '%s' ###" %
+              (len(health_data_file), pattern))
     else:
-        if rampup_value == 0:
-            print("### %d files Selected with pattern '%s' ###" %
-                  (len(health_data_file), pattern))
-        else:
-            print("########## Rampup: %d / %d hosts #########" %
-                  (rampup_value, max_rampup_value))
+        print("########## Rampup: %d / %d hosts #########" %
+              (rampup_value, max_rampup_value))
 
     # Extract data from the hw files
     bench_values = []
@@ -245,12 +245,14 @@ def do_plot(current_dir, gpm_dir, main_title, subtitle, name, unit, titles,
             titles_order, expected_value=""):
     filename = current_dir + "/" + name + ".gnuplot"
     process = subprocess.Popen(["gnuplot", "-V"], stdout=subprocess.PIPE)
-    out, err = process.communicate()
+    out, _ = process.communicate()
     version = int(out.split()[1].split('.')[0])
     if version >= 5:
-        gnuplot_arg = lambda x: "ARG" + str(x + 1)
+        def gnuplot_arg(argument):
+            return "ARG" + str(argument + 1)
     else:
-        gnuplot_arg = lambda x: "'$" + str(x) + "'"
+        def gnuplot_arg(argument):
+            return "'$" + str(argument) + "'"
     with open(filename, "a") as ofile:
         shutil.copyfile("%s/graph2D.gpm" % gpm_dir,
                         "%s/graph2D.gpm" % current_dir)
@@ -326,8 +328,8 @@ def extract_hw_info(hardware, level1, level2, level3):
     for entry in hardware:
         if level2 == '*':
             temp_level2 = entry[1]
-        if (level1 == entry[0] and temp_level2 == entry[1] and
-                level3 == entry[2]):
+        if (level1 == entry[0] and temp_level2 == entry[1]
+                and level3 == entry[2]):
             result.append(entry[3])
     return result
 
@@ -465,10 +467,9 @@ def main():
     detail = {'category': '', 'group': '', 'item': ''}
     global_params = {}
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hp:l:g:c:i:I:r:o:",
-                                   ['pattern', 'log-level', 'group',
-                                    'category', 'item', "ignore",
-                                    "rampup", "output_dir"])
+        opts, _ = getopt.getopt(sys.argv[1:], "hp:l:g:c:i:I:r:o:",
+                                ['pattern', 'log-level', 'group', 'category',
+                                 'item', "ignore", "rampup", "output_dir"])
     except getopt.GetoptError:
         print("Error: One of the options passed "
               "to the cmdline was not supported")
@@ -526,8 +527,7 @@ def main():
             global_params["output_dir"] = arg
 
     if (utils.print_level & utils.Levels.DETAIL) == utils.Levels.DETAIL:
-        if (len(detail['group']) == 0 or len(detail['category']) == 0 or
-                len(detail['item']) == 0):
+        if not detail['group'] or not detail['category'] or not detail['item']:
             print("Error: The DETAIL output requires group, category & item "
                   "options to be set")
             sys.exit(2)
@@ -557,9 +557,9 @@ def main():
             try:
                 if os.path.exists(current_dir):
                     shutil.rmtree(current_dir)
-            except IOError as e:
+            except IOError as myexception:
                 print("Unable to delete directory %s" % current_dir)
-                print(e)
+                print(myexception)
                 sys.exit(2)
 
             temp_rampup_values = [int(name) for name in os.listdir(rampup_dir)
@@ -603,8 +603,8 @@ def main():
 
                 for rampup_value in sorted(rampup_values):
                     metrics = {}
-                    metrics_file = (rampup_dir +
-                                    "/%d/%s/metrics" % (rampup_value, job))
+                    metrics_file = (rampup_dir
+                                    + "/%d/%s/metrics" % (rampup_value, job))
                     if not os.path.isfile(metrics_file):
                         print("Missing metric file for rampup=%d (%s)" %
                               (rampup_value, metrics_file))

hardware/cardiff/check.py
@@ -34,13 +34,13 @@ def search_item(system_list, unique_id, item, regexp, exclude_list=[],
         sets[system[unique_id]] = set()
         current_set = sets[system[unique_id]]
         for stuff in system[item]:
-            m = re.match(regexp, stuff[1])
-            if m:
+            match = re.match(regexp, stuff[1])
+            if match:
                 shall_be_added = False
 
                 # If we have an include_list, only those shall be used
                 # So everything is exclude by default
-                if len(include_list) > 0:
+                if include_list:
                     shall_be_excluded = True
                 else:
                     shall_be_excluded = False
@@ -108,19 +108,19 @@ def compute_deviance_percentage(item, df):
 
 
 def print_detail(detail_options, details, df, matched_category):
-    if (utils.print_level & utils.Levels.DETAIL) != utils.Levels.DETAIL:
+    if (utils.PRINTLEVEL & utils.Levels.DETAIL) != utils.Levels.DETAIL:
         return
-    if len(df.loc[details]) > 0:
+    if df.loc[details]:
         print()
         print("%-34s: %-8s: %s" % (matched_category[0],
-                                   utils.Levels.message[utils.print_level],
+                                   utils.Levels.message[utils.PRINTLEVEL],
                                    detail_options['item']))
         print(df.loc[details])
 
 
 def prepare_detail(detail_options, group_number, category, item, details,
                    matched_category_to_save):
-    if (utils.print_level & utils.Levels.DETAIL) != utils.Levels.DETAIL:
+    if (utils.PRINTLEVEL & utils.Levels.DETAIL) != utils.Levels.DETAIL:
         return
 
     matched_group = ''
@@ -144,12 +144,12 @@ def prepare_detail(detail_options, group_number, category, item, details,
     elif re.search(detail_options['item'], item) is not None:
         matched_item = re.search(detail_options['item'], item).group()
 
-    if (len(matched_group) > 0 and len(matched_category) > 0 and
-            len(matched_item) > 0):
+    if matched_group and matched_category and matched_item:
         details.append(matched_item)
         if matched_category not in matched_category_to_save:
             matched_category_to_save.append(matched_category)
         return matched_category
+
     return ""
 
 
@@ -220,7 +220,7 @@ def logical_disks_perf(system_list, unique_id, group_number, detail_options,
             if perf[2] not in modes and perf_unit in perf[2]:
                 modes.append(perf[2])
 
-    if len(modes) == 0:
+    if modes:
         return
 
     for mode in sorted(modes):
@@ -415,7 +415,7 @@ def print_perf(tolerance_min, tolerance_max, item, df, mode, title,
 
 
 def print_summary(mode, array, array_name, unit, df, item_value=None):
-    if (utils.print_level & utils.Levels.SUMMARY) and (len(array) > 0):
+    if (utils.PRINTLEVEL & utils.Levels.SUMMARY) and array:
         result = []
         before = ""
         after = ""
@@ -437,7 +437,7 @@ def print_summary(mode, array, array_name, unit, df, item_value=None):
         perf_status = ""
         if array_name == "consistent":
             if item_value is not None:
-                if mode == "loops_per_sec" or mode == "bogomips":
+                if mode in ("loops_per_sec", "bogomips"):
                     min_cpu_perf = perf_cpu_tables.get_cpu_min_perf(mode,
                                                                     item_value)
                     if min_cpu_perf == 0:
@@ -447,8 +447,7 @@ def print_summary(mode, array, array_name, unit, df, item_value=None):
                         perf_status = ": " + GREEN + "PERF OK" + WHITE
                     else:
                         perf_status = (": " + RED + "PERF FAIL" + WHITE +
-                                       " as min perf should have been : " +
-                                       str(min_cpu_perf))
+                                       " as min perf should have been : " + str(min_cpu_perf))
         utils.do_print(
             mode, utils.Levels.SUMMARY,
             "%3d %s%-10s%s hosts with %8.2f %-4s as average value and %8.2f "

hardware/cardiff/compare_sets.py
@@ -111,16 +111,16 @@ def compute_similar_hosts_list(systems_groups, new_groups):
     for group in new_groups:
         for systems_group in systems_groups:
             intersection = set.intersection(systems_group, group)
-            if (len(intersection) < len(systems_group) and
-                    len(intersection) > 0):
-                # print("%d vs %d" % (len(intersection), len(systems_group)))
-                # We do have a partial match meaning we shall break
-                # the existing group in pieces
-                difference = set.difference(systems_group, group)
-                # The group we worked on doesn't exist anymore
-                # So let's delete it
-                systems_groups.remove(systems_group)
+            if intersection:
+                if len(intersection) < len(systems_group):
+                    # print("%d vs %d" % (len(intersection), len(systems_group)))
+                    # We do have a partial match meaning we shall break
+                    # the existing group in pieces
+                    difference = set.difference(systems_group, group)
+                    # The group we worked on doesn't exist anymore
+                    # So let's delete it
+                    systems_groups.remove(systems_group)
 
-                # Let's add the two sub groups generated by this split
-                systems_groups.append(intersection)
-                systems_groups.append(difference)
+                    # Let's add the two sub groups generated by this split
+                    systems_groups.append(intersection)
+                    systems_groups.append(difference)

commit f9f94bec9d24d074e13f8ee0dd35eb5834f31a32

hardware/megacli.py: Fixing typo
Commit 54a78530fca3bfe81cfa54fc96e19ee7688280ca introduced a typo which broken the code as in :
	Traceback (most recent call last):
	  File "/usr/bin/hardware-detect", line 10, in <module>
	    sys.exit(main())
	  File "/usr/lib/python2.7/site-packages/hardware/detect.py", line 996, in main
	    _main(options)
	  File "/usr/lib/python2.7/site-packages/hardware/detect.py", line 921, in _main
	    detect_megacli(hrdw)
	  File "/usr/lib/python2.7/site-packages/hardware/detect.py", line 170, in detect_megacli
	    ctrl_num = megacli.adp_count()
	  File "/usr/lib/python2.7/site-packages/hardware/megacli.py", line 148, in adp_count
	    arr = run_and_parse('adpCount')
	  File "/usr/lib/python2.7/site-packages/hardware/megacli.py", line 142, in run_and_parse
	    res = run_megacli(*args)
	  File "/usr/lib/python2.7/site-packages/hardware/megacli.py", line 127, in run_megacli
	    prog_exec = search_exec(["megacli", "MegaCli", "MegaCli64"])
	  File "/usr/lib/python2.7/site-packages/hardware/megacli.py", line 84, in search_exec
	    prog_path = which(prog_name)
	  File "/usr/lib/python2.7/site-packages/hardware/megacli.py", line 76, in which
	    if _access_check(name, mode):
	  File "/usr/lib/python2.7/site-packages/hardware/megacli.py", line 46, in _access_check
	    return os.path.exists(myfile and os.access(myfile, mode) and not os.path.isdir(myfile))
	  File "/usr/lib64/python2.7/genericpath.py", line 18, in exists
	    os.stat(path)
	TypeError: coercing to Unicode: need string or buffer, bool found

The misplaced parenthesis is the cause of that.

Thanks Yatin Karel for reporting.

Signed-off-by: Erwan Velu <e.velu@criteo.com>

hardware/megacli.py
@@ -43,7 +43,7 @@ def which(cmd, mode=os.F_OK | os.X_OK, path=None):
     # Additionally check that `file` is not a directory, as on Windows
     # directories pass the os.access check.
     def _access_check(myfile, mode):
-        return os.path.exists(myfile and os.access(myfile, mode) and not os.path.isdir(myfile))
+        return os.path.exists(myfile) and os.access(myfile, mode) and not os.path.isdir(myfile)
 
     # If we're given a path with a directory part, look it up directly rather
     # than referring to PATH directories. This includes checking relative to