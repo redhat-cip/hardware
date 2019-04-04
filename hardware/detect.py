337fa06994d0375da3586fbce30d18cc1a40361b
Tony Breeds <tony@bakeyournoodle.com>
2019-04-05 10:25:59 +1100

    Don't repeat the same string formatting
    Repeating "physical_{}".format(processor) leads to longer lines that
    need breaking.  Rather than repeat that string operation save it in a
    temporary variable and use that.
    
    We don't strictly need to do the same thing for NUMA and logical tags but
    to keep the sections following the same code style we do it there too.

--- hardware/detect.py
+++ hardware/detect.py
@@ -721,49 +721,45 @@ def get_cpus(hw_lst):
 
     hw_lst.append(("cpu", "physical", "number", int(lscpu["Socket(s)"])))
     for processor in range(int(lscpu["Socket(s)"])):
+        ptag = "physical_{}".format(processor)
         (exists, value) = _from_file("/sys/devices/system/cpu/cpufreq/boost",
                                      mapping={'1': 'enabled'},
                                      default='disabled')
         if exists:
             hw_lst.append(
-                ('cpu', "physical_{}".format(processor), 'boost', value))
+                ('cpu', ptag, 'boost', value))
 
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'vendor', lscpu['Vendor ID']))
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'product', lscpu['Model name']))
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'cores', int(lscpu['Core(s) per socket'])))
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'threads', int(lscpu['Thread(s) per core']) * int(lscpu['Core(s) per socket'])))
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'family', int(lscpu['CPU family'])))
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'model', int(lscpu['Model'])))
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'stepping', int(lscpu['Stepping'])))
+        hw_lst.append(('cpu', ptag, 'vendor', lscpu['Vendor ID']))
+        hw_lst.append(('cpu', ptag, 'product', lscpu['Model name']))
+        hw_lst.append(('cpu', ptag, 'cores',
+                       int(lscpu['Core(s) per socket'])))
+        hw_lst.append(('cpu', ptag, 'threads',
+                       (int(lscpu['Thread(s) per core']) *
+                        int(lscpu['Core(s) per socket']))))
+        hw_lst.append(('cpu', ptag, 'family', int(lscpu['CPU family'])))
+        hw_lst.append(('cpu', ptag, 'model', int(lscpu['Model'])))
+        hw_lst.append(('cpu', ptag, 'stepping', int(lscpu['Stepping'])))
         for item in ['L1d cache', 'L1i cache', 'L2 cache', 'L3 cache']:
             if item in lscpu:
-                hw_lst.append(('cpu', "physical_{}".format(
-                    processor), item.lower(), lscpu[item]))
+                hw_lst.append(('cpu', ptag, item.lower(), lscpu[item]))
         if 'CPU min MHz' in lscpu:
-            hw_lst.append(('cpu', "physical_{}".format(
-                processor), 'min_Mhz', float(lscpu['CPU min MHz'])))
+            hw_lst.append(('cpu', ptag, 'min_Mhz',
+                           float(lscpu['CPU min MHz'])))
         if 'CPU max MHz' in lscpu:
-            hw_lst.append(('cpu', "physical_{}".format(
-                processor), 'max_Mhz', float(lscpu['CPU max MHz'])))
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'current_Mhz', float(lscpu['CPU MHz'])))
-        hw_lst.append(('cpu', "physical_{}".format(
-            processor), 'flags', lscpu['Flags']))
+            hw_lst.append(('cpu', ptag, 'max_Mhz',
+                           float(lscpu['CPU max MHz'])))
+        hw_lst.append(('cpu', ptag, 'current_Mhz',
+                       float(lscpu['CPU MHz'])))
+        hw_lst.append(('cpu', ptag, 'flags', lscpu['Flags']))
 
     hw_lst.append(('cpu', 'logical', 'number', int(lscpu['CPU(s)'])))
     # Governors could be different on logical cpus
     for cpu in range(int(lscpu['CPU(s)'])):
+        ltag = "logical_{}".format(cpu)
         (exists, value) = _from_file(("/sys/devices/system/cpu/cpufreq/"
                                       "policy{}/scaling_governor".format(cpu)))
         if exists:
-            hw_lst.append(('cpu', "logical_{}".format(cpu), "governor", value))
+            hw_lst.append(('cpu', ltag, "governor", value))
 
     # Extracting numa nodes
     hw_lst.append(('numa', 'nodes', 'count', int(lscpu['NUMA node(s)'])))
@@ -779,6 +775,7 @@ def get_cpus(hw_lst):
     # on that.
     numa_nodes.sort(key=lambda t: t[1])
     for (key, node_id) in numa_nodes:
+        ntag = 'node_{}'.format(node_id)
         cpus = lscpu[key]
         # lscpu -x provides the cpu mask
         cpu_mask = lscpux[key]
@@ -803,10 +800,8 @@ def get_cpus(hw_lst):
                         total_cpus = total_cpus + max_cpu - min_cpu + 1
 
         # total_cpus = 12 for "0-5,48-53"
-        hw_lst.append(('numa', 'node_{}'.format(
-            node_id), 'cpu_count', total_cpus))
-        hw_lst.append(('numa', 'node_{}'.format(
-            node_id), 'cpu_mask', cpu_mask))
+        hw_lst.append(('numa', ntag, 'cpu_count', total_cpus))
+        hw_lst.append(('numa', ntag, 'cpu_mask', cpu_mask))
 
 
 def fix_bad_serial(hw_lst, uuid, mobo_id, nic_id):
