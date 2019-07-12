868a5cf1dd23e37c3895453beccda19b7ebba1e0
Riccardo Pittau <elfosardo@gmail.com>
2019-07-12 09:47:57 +0200

    [BUGZILLA 1729237] Fix benchmark output for disks
    when running the benchmark script with python 3, the command
    output is bytecode and not str.
    This patch fixes the parsing converting the output in the
    appropriate format according to Python version used.

--- hardware/tests/test_benchmark_disk.py
+++ hardware/tests/test_benchmark_disk.py
@@ -24,7 +24,24 @@ from hardware.benchmark import disk
 
 
 FIO_OUTPUT_READ = """MYJOB-fake-disk: (groupid=0, jobs=1): err= 0: pid=5427:
-  read : io=123456KB, bw=123456KB/s, iops=123, runt= 10304msec""".splitlines()
+  read : io=123456KB, bw=123456KB/s, iops=123, runt= 10304msec"""
+
+DISK_PERF_EXPECTED = [
+    ('disk', 'fake-disk', 'size', '10'),
+    ('disk', 'fake-disk2', 'size', '15'),
+    ('disk', 'fake-disk', 'standalone_read_1M_KBps', '123456'),
+    ('disk', 'fake-disk', 'standalone_read_1M_IOps', '123'),
+    ('disk', 'fake-disk', 'standalone_randread_4k_KBps', '123456'),
+    ('disk', 'fake-disk', 'standalone_randread_4k_IOps', '123'),
+    ('disk', 'fake-disk', 'standalone_read_1M_KBps', '123456'),
+    ('disk', 'fake-disk', 'standalone_read_1M_IOps', '123'),
+    ('disk', 'fake-disk', 'standalone_randread_4k_KBps', '123456'),
+    ('disk', 'fake-disk', 'standalone_randread_4k_IOps', '123'),
+    ('disk', 'fake-disk', 'simultaneous_read_1M_KBps', '123456'),
+    ('disk', 'fake-disk', 'simultaneous_read_1M_IOps', '123'),
+    ('disk', 'fake-disk', 'simultaneous_randread_4k_KBps', '123456'),
+    ('disk', 'fake-disk', 'simultaneous_randread_4k_IOps', '123'),
+]
 
 
 @mock.patch.object(subprocess, 'Popen')
@@ -35,38 +52,28 @@ class TestBenchmarkDisk(unittest.TestCase):
         self.hw_data = [('disk', 'fake-disk', 'size', '10'),
                         ('disk', 'fake-disk2', 'size', '15')]
 
-    def test_disk_perf(self, mock_popen):
-        mock_popen.return_value = mock.Mock(stdout=FIO_OUTPUT_READ)
+    def test_disk_perf_text(self, mock_popen):
+        mock_popen.return_value = mock.Mock(
+            stdout=FIO_OUTPUT_READ.splitlines())
         disk.disk_perf(self.hw_data)
+        self.assertEqual(sorted(DISK_PERF_EXPECTED), sorted(self.hw_data))
 
-        expected = [
-            ('disk', 'fake-disk', 'size', '10'),
-            ('disk', 'fake-disk2', 'size', '15'),
-            ('disk', 'fake-disk', 'standalone_read_1M_KBps', '123456'),
-            ('disk', 'fake-disk', 'standalone_read_1M_IOps', '123'),
-            ('disk', 'fake-disk', 'standalone_randread_4k_KBps', '123456'),
-            ('disk', 'fake-disk', 'standalone_randread_4k_IOps', '123'),
-            ('disk', 'fake-disk', 'standalone_read_1M_KBps', '123456'),
-            ('disk', 'fake-disk', 'standalone_read_1M_IOps', '123'),
-            ('disk', 'fake-disk', 'standalone_randread_4k_KBps', '123456'),
-            ('disk', 'fake-disk', 'standalone_randread_4k_IOps', '123'),
-            ('disk', 'fake-disk', 'simultaneous_read_1M_KBps', '123456'),
-            ('disk', 'fake-disk', 'simultaneous_read_1M_IOps', '123'),
-            ('disk', 'fake-disk', 'simultaneous_randread_4k_KBps', '123456'),
-            ('disk', 'fake-disk', 'simultaneous_randread_4k_IOps', '123')
-        ]
-        self.assertEqual(sorted(expected), sorted(self.hw_data))
+    def test_disk_perf_bytes(self, mock_popen):
+        mock_popen.return_value = mock.Mock(
+            stdout=FIO_OUTPUT_READ.encode().splitlines())
+        disk.disk_perf(self.hw_data)
+        self.assertEqual(sorted(DISK_PERF_EXPECTED), sorted(self.hw_data))
 
     def test_get_disks_name(self, mock_popen):
         result = disk.get_disks_name(self.hw_data)
         self.assertEqual(sorted(['fake-disk', 'fake-disk2']), sorted(result))
 
     def test_run_fio(self, mock_popen):
-        mock_popen.return_value = mock.Mock(stdout=FIO_OUTPUT_READ)
+        mock_popen.return_value = mock.Mock(
+            stdout=FIO_OUTPUT_READ.splitlines())
         hw_data = []
         disks_list = ['fake-disk', 'fake-disk2']
         disk.run_fio(hw_data, disks_list, "read", 123, 10, 5)
-
         self.assertEqual(sorted(
             [('disk', 'fake-disk', 'simultaneous_read_123_KBps', '123456'),
              ('disk', 'fake-disk', 'simultaneous_read_123_IOps', '123')]),
